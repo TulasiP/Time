04f71040a301c70d0064deac523e9f74a8821f44
TulasiP <meghana.tulasi@yahoo.com>
2016-08-07 04:54:02 +0530

    Added existing project files
    Second upload

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt;
+            if (DateTime.TryParseExact("24-01-2013",
+                                        "dd/MM/yyyy",
+                                        CultureInfo.InvariantCulture,
+                                        DateTimeStyles.None,
+                out dt))
+            {
+                Console.WriteLine(dt.ToShortDateString());
+            }
+            else
+            {
+                Console.WriteLine("String is not formated to date");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
